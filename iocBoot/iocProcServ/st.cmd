542126c12d3ba204d9dd59d238345388b7301787
Casper Ti. Vector <CasperVector@gmail.com>
2022-11-07 09:18:37 +0800

    Assume ${P} already has the trailing colon.

--- iocBoot/iocProcServ/st.cmd
+++ iocBoot/iocProcServ/st.cmd
@@ -6,10 +6,10 @@ dbLoadDatabase "dbd/procServControlApp.dbd"
 procServControlApp_registerRecordDeviceDriver(pdbbase)
 
 cd "${TOP}/iocBoot/${IOC}"
-iocshLoad("ioc-decl.cmd", "PORT=port1,P=ProcServ:IOC01,DEVICE=localhost:7001")
-iocshLoad("ioc-decl.cmd", "PORT=port2,P=ProcServ:IOC02,DEVICE=localhost:7002")
+iocshLoad("ioc-decl.cmd", "PORT=port1,P=ProcServ:IOC01:,DEVICE=localhost:7001")
+iocshLoad("ioc-decl.cmd", "PORT=port2,P=ProcServ:IOC02:,DEVICE=localhost:7002")
 
 iocInit()
-seq(procServControl, "P=ProcServ:IOC01")
-seq(procServControl, "P=ProcServ:IOC02")
+seq(procServControl, "P=ProcServ:IOC01:")
+seq(procServControl, "P=ProcServ:IOC02:")
 
